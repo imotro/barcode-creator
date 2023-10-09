179869146740e14eae5b01d91442ac8b48c6cb0b
imotro <139806892+imotro@users.noreply.github.com>
2023-10-09 18:31:24 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Enter text to create barcode');
+  document.onkeypress = () =>{document.querySelector('h1').classList.add("encode")}
+</script>
+
+<template>
+  <input v-model="msg">
+  <br>
+  <center><h1>{{ msg }}</h1></center>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Libre+Barcode+39&display=swap');
+h1.encode{
+   font-family: 'Libre Barcode 39';
+   font-size:100px;
+}
+</style>
